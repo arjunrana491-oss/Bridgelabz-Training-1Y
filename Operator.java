26dcfddb16f6d6ac81a4416417883552955f1faa
arjunrana491-oss <arjun.rana491@gmail.com>
2026-01-22 15:26:25 +0530

    commond line argument

---
+++ Operator.java
@@ -0,0 +1,28 @@
+import java.util.scanner;
+public class Operator{
+public void main(string[] args){
+int i=0;
+system.out.println("val : "+i);
+i++;
+system.out.println("val a : "i++);
+system.out.println("val b : "+i);
+i++;
+system.out.println("val c : "+i);
+system.out.println("val d : "+i++);
+int z = i-i++ - --i +i -++i + i++;
+system.out.println("z");
+system.out.println("i");
+for(int i=0;i<10;i++){
+system.out.println("i: "+i);
+}
+int j=10;
+while(j>0){
+system.out.println("l: "+i);
+}
+int k=10;
+do{
+system.out.println("k: "+k);
+k--;
+}
+while(k>0);
+}
